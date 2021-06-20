34749c93926e55062306db754aee09420890e38b
RicardoSoz <a01420312@itesm.mx>
2021-06-20 14:50:40 -0500

    Fixed Jump and animations

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,7 +9,12 @@ public class PlayerMove : MonoBehaviour{
     public float fallMultiplier = 0.5f;
     public float lowJumpMultiplier = 1f;
     public bool betterJump = false;
+
     Rigidbody2D rigibody2D;
+    //Arrastrar el SpriteRenderer al menu del srcipt dentro de Frog para cambiar la orentación de la animación 
+    public SpriteRenderer spriteRenderer;
+    //Arrastrar el Animator al menu del script dentro de Frog para pasar de Idle a Run
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start(){
@@ -19,25 +24,59 @@ public class PlayerMove : MonoBehaviour{
     // Update is called once per frame
     void FixedUpdate(){
 
-        if (Input.GetKey("d")){
-            //Left
+        //Right--------------------------------------------------------------------------------------------
+        if (Input.GetKey("d"))
+        {
             rigibody2D.velocity = new Vector2(runSpeed, rigibody2D.velocity.y);
-
-        } 
-        else if (Input.GetKey("a")){
-            //Right
-            rigibody2D.velocity = new Vector2(-runSpeed, rigibody2D.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Run", true);
         }
-        else if (Input.GetKey("w") && CheckGround.isGrounded)
-        {   //Jump
+        //else if (Input.GetKey("d") && Input.GetKey("w") && CheckGround.isGrounded)
+        //{
+        //    rigibody2D.velocity = new Vector2(runSpeed, rigibody2D.velocity.y);
+        //    rigibody2D.velocity = new Vector2(-rigibody2D.velocity.x, jumpSpeed);
+        //    spriteRenderer.flipX = false;
+        //}
+
+        //Left--------------------------------------------------------------------------------------------
+        else if (Input.GetKey("a"))
+        {
+            rigibody2D.velocity = new Vector2(-runSpeed, rigibody2D.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Run", true);
+        }
+        //else if (Input.GetKey("a") && Input.GetKey("w") && CheckGround.isGrounded)
+        //{
+        //    rigibody2D.velocity = new Vector2(-runSpeed, rigibody2D.velocity.y);
+        //    rigibody2D.velocity = new Vector2(-rigibody2D.velocity.x, jumpSpeed);
+        //    spriteRenderer.flipX = true;
+        //}
+        //Static--------------------------------------------------------------------------------------------
+        else
+        {
+            rigibody2D.velocity = new Vector2(0, rigibody2D.velocity.y);
+            animator.SetBool("Run", false);
+        }
+
+        //Jump--------------------------------------------------------------------------------------------
+        if (Input.GetKey("w") && CheckGround.isGrounded)
+        {
             rigibody2D.velocity = new Vector2(-rigibody2D.velocity.x,jumpSpeed);
         }
-        else
-        {   //Static
-            rigibody2D.velocity = new Vector2(0, rigibody2D.velocity.y);
+        //Jump animation
+        if (CheckGround.isGrounded)
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        if (!CheckGround.isGrounded)
+        {
+            animator.SetBool("Run", false);
+            animator.SetBool("Jump", true);
         }
 
 
+        //Increase the jump
         if (betterJump)
         {
             if (rigibody2D.velocity.y < 0)
